3252accfc7f2966d4337f34e0ff278c740f6ac74
SalinasJason <jasonsalinas770@gmail.com>
2025-02-19 15:02:42 -0800

    Created a abstract class named Animal and added private fields,public properties and abstract method.

---
+++ COMP003A.ZooManagementSystem/Animal.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.ZooManagementSystem
+{
+    internal abstract class Animal
+    {
+        private string _name;
+        private string _species;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    _name = value;
+                }
+            }
+        }
+
+
+        public string Species
+        {
+            get { return _species; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    _species = value;
+                }
+            }
+        }
+
+        public abstract void MakeSound();
+    }
+}
