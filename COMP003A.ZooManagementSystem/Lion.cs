cde6cd4c7b2b22849ea17ace84f26b0d7d509def
SalinasJason <jasonsalinas770@gmail.com>
2025-02-20 13:31:32 -0800

    Created two classes named Lion and Parrot that inherits from Animal and overrides the MakeSound() method

---
+++ COMP003A.ZooManagementSystem/Lion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using COMP003A.ZooManagementSystem;
+
+namespace COMP003A.ZooManagementSystem
+{
+    internal class Lion : Animal
+    {
+        public Lion (string LionName, string LionSpecies) 
+        {
+            Name = LionName;
+            Species = LionSpecies;
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine($"The lion roars! ({Name}, {Species})");
+        }
+    }
+}
---
+++ COMP003A.ZooManagementSystem/Parrot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace COMP003A.ZooManagementSystem
+{
+    internal class Parrot : Animal
+    {
+        public Parrot(string ParrotName, string ParrotSpecies)
+        {
+            Name = ParrotName;
+            Species = ParrotSpecies;
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine($"The parrot squawks! ({Name}, {Species})");
+        }
+    }
+}
