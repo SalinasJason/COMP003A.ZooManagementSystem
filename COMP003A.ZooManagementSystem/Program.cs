07026e33914075553c8e212872e2f27a1ac63924
SalinasJason <jasonsalinas770@gmail.com>
2025-02-20 15:27:16 -0800

    Added a menu

--- COMP003A.ZooManagementSystem/Program.cs
+++ COMP003A.ZooManagementSystem/Program.cs
@@ -8,7 +8,17 @@ namespace COMP003A.ZooManagementSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Zoo Management System!");
+            Console.WriteLine("\nPlease choose an option:");
+            Console.WriteLine("1. Add a Lion");
+            Console.WriteLine("2. Add a Parrot");
+            Console.WriteLine("3. Display All Animals");
+            Console.WriteLine("4. Describe an Aniaml");
+            Console.WriteLine("5. Exit");
+
+            int choice = 0;
+            Console.Write("\nYour choice: ");
+            choice = int.Parse(Console.ReadLine()); 
         }
     }
 }
